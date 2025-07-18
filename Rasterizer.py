f896253116a77da24b2a53298023df7edfec90c3
Estuardo Castro <cas23890@uvg.edu.gt>
2025-07-17 21:32:45 -0600

    Terminar sin modelo

--- Rasterizer.py
+++ Rasterizer.py
@@ -1,63 +1,95 @@
 import pygame
-from gl import Renderer
+from gl import *
 from BMP_Writer import GenerateBMP
+from model import Model
+from shaders import *
+from objloader import cargar_modelo
+from MathLib import auto_fit_model
+from shaders import vertexShader
 
-width = 1920//2
-height = 1080//2
+width = 720
+height = 480
 
 screen = pygame.display.set_mode((width, height), pygame.SCALED)
-reloj = pygame.time.Clock()
+clock = pygame.time.Clock()
 
 rend = Renderer(screen)
 
-rend.glLimpiarColor(0, 0, 0)
 
-polygons = [
-    #Coordenadas para todos los polígonos
+triangleModel = Model()
+triangleModel.vertices = cargar_modelo("")
+auto_fit_model(triangleModel, width, height)
+rend.models.append(triangleModel)
 
-    # Polígono 1
-    [(165, 380), (185, 360), (180, 330), (207, 345),(233, 330), (230, 360), (250, 380), (220, 385), (205, 410), (193, 383)],
-    # Polígono 2
-    [(321, 335), (288, 286), (339, 251), (374, 302)],
-    # Polígono 3
-    [(377, 249), (411, 197), (436, 249)],
-    # Polígono 4
-    [(413, 177), (448, 159), (502, 88),  (553, 53), (535, 36),  (676, 37),  (660, 52),  (750, 145), (761, 179), (672, 192), (659, 214), (615, 214), (632, 230), (580, 230), (597, 215), (552, 214), (517, 144), (466, 180)],
-]
+triangleModel.vertexShader = vertexShader
 
-# Polígono 5
-hoyo = [
-    (682, 175), (708, 120),
-    (735, 148), (739, 170),
-]
 
-colors = [
-    (0.5, 1, 0.5),
-    (1, 0.55, 0),
-    (1, 0.4, 0.4),
-    (1, 0.84, 0),
-]
 
 isRunning = True
 while isRunning:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            isRunning = False
-    
-    rend.glLimpiar()
 
-    for i, poly in enumerate(polygons):
-        rend.glColor(*colors[i])
-        rend.glLlenarPoligono(poly)
-        rend.glColor(1, 1, 1)
-        rend.glPoligono(poly)
+	deltaTime = clock.tick(60) / 1000.0
 
-    rend.glLlenarPoligono(hoyo, rend.clearColor)
-    rend.glColor(1, 1, 1)
-    rend.glPoligono(hoyo)
 
-    pygame.display.flip()
-    reloj.tick(60)
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			isRunning = False
+
+		elif event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_1:
+				rend.primitiveType = POINTS
+
+			elif event.key == pygame.K_2:
+				rend.primitiveType = LINES
+
+			elif event.key == pygame.K_3:
+				rend.primitiveType = TRIANGLES
+
+
+
+	keys = pygame.key.get_pressed()
+
+	if keys[pygame.K_RIGHT]:
+		triangleModel.translation[0] += 10 * deltaTime
+	if keys[pygame.K_LEFT]:
+		triangleModel.translation[0] -= 10 * deltaTime
+	if keys[pygame.K_UP]:
+		triangleModel.translation[1] += 10 * deltaTime
+	if keys[pygame.K_DOWN]:
+		triangleModel.translation[1] -= 10 * deltaTime
+
+
+	if keys[pygame.K_d]:
+		triangleModel.rotation[2] += 20 * deltaTime
+	if keys[pygame.K_a]:
+		triangleModel.rotation[2] -= 20 * deltaTime
+
+	if keys[pygame.K_w]:
+		triangleModel.scale =  [(i + deltaTime) for i in triangleModel.scale]
+	if keys[pygame.K_s]:
+		triangleModel.scale = [(i - deltaTime) for i in triangleModel.scale ]
+
+	rend.glClear()
+
+	rend.glRender()
+
+	pygame.display.flip()
+
 
 GenerateBMP("output.bmp", width, height, 3, rend.frameBuffer)
+# Definimos (modo, nombre_de_archivo)
+capturas = [
+    (POINTS,    "output_points.bmp"),
+    (LINES,     "output_lines.bmp"),
+    (TRIANGLES, "output_wireframe.bmp"),
+    (TRIANGLES, "output_filled.bmp"),  # si quisieras un 4º distinto, p. ej. con otro shader
+]
+
+for modo, fname in capturas:
+    rend.primitiveType = modo
+    rend.glClear()           
+    rend.glRender()         
+    GenerateBMP(fname, width, height, 3, rend.frameBuffer)
+
+
 pygame.quit()
